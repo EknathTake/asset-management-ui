42c6fcc769a06b3f93013fab9dd1284a212eaa2f
EknathUdhav.Take@idemia.com <1N@th@1202>
2019-11-12 15:52:08 +0530

    login

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,38 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {Routes, RouterModule} from '@angular/router';
+import {LoginComponent} from './user/login/login.component';
+import {HomePageComponent} from './user/home-page/home-page.component';
+import {AssetEntryComponent} from './asset/asset-entry/asset-entry.component';
+import {AssetListComponent} from './asset/asset-list/asset-list.component';
+import {AssetSummaryComponent} from './asset/asset-summary/asset-summary.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [{
+  path: '',
+  component: LoginComponent
+}, {
+  path: 'login',
+  component: LoginComponent
+}, {
+  path: 'register',
+  component: LoginComponent
+}, {
+  path: 'home',
+  component: AssetListComponent
+}, {
+  path: 'asset/create',
+  component: AssetEntryComponent
+}, {
+  path: 'asset/list',
+  component: AssetListComponent
+}, {
+  path: 'asset/summary',
+  component: AssetSummaryComponent
+}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,40 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {UserModule} from './user/user.module';
+import {MatButtonModule, MatFormFieldModule, MatIconModule, MatMenuModule, MatSelectModule, MatToolbarModule} from '@angular/material';
+import {FormsModule} from '@angular/forms';
+import {NavbarComponent} from './navbar/navbar.component';
+import {HTTP_INTERCEPTORS, HttpClientModule} from '@angular/common/http';
+import {CookieService} from 'ngx-cookie-service';
+import {MessageDialogComponent} from './shared/message-dialog/message-dialog.component';
+import {DataTablesModule} from 'angular-datatables';
+import {SharedModule} from './shared/shared.module';
+import {MaterialModule} from './material/material.module';
+import {AssetModule} from './asset/asset.module';
+import {RequestLogInterceptor} from './shared/request-log-interceptor';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    MessageDialogComponent
   ],
   imports: [
+    FormsModule,
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    UserModule,
+    HttpClientModule,
+    SharedModule,
+    MaterialModule
   ],
-  providers: [],
+  providers: [CookieService, {provide: HTTP_INTERCEPTORS, useClass: RequestLogInterceptor, multi: true}],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
