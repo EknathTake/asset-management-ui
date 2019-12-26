a91729785fd6ef0599b65ec7d8ded64f1b7adab8
EknathUdhav.Take@idemia.com <1N@th@1202>
2019-12-26 23:33:29 +0530

    UI Beautification

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -2,6 +2,12 @@ import {Component, OnInit} from '@angular/core';
 import {UserAuthService} from '../services/userauth.service';
 import {Router} from '@angular/router';
 import {CookieService} from 'ngx-cookie-service';
+import {AssetDetails} from '../shared/model/asset-details';
+import {AssetResponse} from '../shared/model/asset-response';
+import {ExcelData} from '../shared/model/excel-data';
+import {MatTableDataSource} from '@angular/material';
+import {AssetService} from '../services/asset.service';
+import {ExcelService} from '../services/excel.service';
 
 @Component({
   selector: 'app-navbar',
@@ -11,8 +17,16 @@ import {CookieService} from 'ngx-cookie-service';
 export class NavbarComponent implements OnInit {
 
   isLogedIn: boolean;
+  assetSummary: AssetDetails[] = [];
+  total = 0;
+  total4GB = 0;
+  total8GB = 0;
+  total16GB = 0;
+  public assetResponse = new Array();
 
-  constructor(private userAuth: UserAuthService, private router: Router, private cookieService: CookieService) {
+  constructor(private userAuth: UserAuthService,
+              private router: Router, private cookieService: CookieService,
+              private assetService: AssetService, private excelService: ExcelService) {
     router.events.subscribe((val) => {
 
       if (this.userAuth.currentUserValue) {
@@ -25,6 +39,42 @@ export class NavbarComponent implements OnInit {
     if (this.userAuth.currentUserValue) {
       this.isLogedIn = true;
     }
+
+    this.assetService.getSummary().subscribe(summary => {
+      summary.forEach(ss => {
+        this.assetSummary.push(ss.assetDetails);
+        this.total += ss.assetDetails.total;
+        this.total4GB += ss.assetDetails.ram4GB;
+        this.total8GB += ss.assetDetails.ram8GB;
+        this.total16GB += ss.assetDetails.ram16GB;
+      });
+
+      this.assetSummary.push(new AssetDetails('Total', '', this.total4GB, this.total8GB, this.total16GB, this.total));
+    });
+
+    this.assetService.getAllAsset().subscribe(response => {
+      const res = response as AssetResponse;
+      let cnt = 1;
+      res.data.forEach(value => {
+        const excelData = new ExcelData();
+        excelData.sNo = cnt++;
+        excelData.empId = value.employee.empId;
+        excelData.name = value.employee.firstName + ' ' + value.employee.lastName;
+        excelData.location = value.employee.location;
+        excelData.costCenter = value.employee.costCenter;
+        excelData.productLine = value.employee.productLine;
+        excelData.jobRole = value.employee.jobRole;
+        excelData.technology = value.employee.technology;
+        excelData.model = value.model;
+        excelData.ram = value.ram;
+        excelData.serialNumber = value.serialNumber;
+        excelData.assetTag = value.assetTag;
+        excelData.dateAllocated = value.dateAllocated;
+        excelData.hostname = value.hostname;
+        excelData.status = value.status;
+        this.assetResponse.push(excelData);
+      });
+    });
   }
 
   logout() {
@@ -32,5 +82,9 @@ export class NavbarComponent implements OnInit {
     location.href = 'http://localhost:4200/login';
   }
 
+  exportToExcel() {
+    // this.excelService.exportAsExcelFile(this.assetResponse, 'Enventory_Details');
+    this.excelService.toFile(this.assetResponse, this.assetSummary, 'Enventory_Details');
+  }
 
 }
